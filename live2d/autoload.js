9bca08df55ff0fae645edf4e360ce33589643e30
juezho <1499475022@qq.com>
2020-09-27 10:13:07 +0800

    12

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,6 +1,6 @@
 // 加载 CSS
 $("<link>").attr({
-    href: "//cdn.jsdelivr.net/gh/juezho/cdn/live2d/waifu.min.css",
+    href: "//cdn.jsdelivr.net/gh/juezho/cdn/live2d/waifu.css",
     rel: "stylesheet",
     type: "text/css"
 }).appendTo('head');
@@ -19,10 +19,14 @@ $.ajax({
     url: '//cdn.jsdelivr.net/gh/juezho/cdn/live2d/waifu-tips.min.js',
     dataType: "script",
     cache: true,
-    async: false,
-    success: function () {
-        // 初始化看板娘，加载 waifu-tips.json
+    async: false
+});
 
+
+!function live2d_try() {
+    // 初始化看板娘，加载 waifu-tips.json
+    try {
+        live2d_settings && loadlive2d;
         /* 可直接修改部分参数 */
         live2d_settings['modelId'] = 1; // 默认模型 ID
         live2d_settings['modelTexturesId'] = 87; // 默认材质 ID
@@ -33,11 +37,14 @@ $.ajax({
         live2d_settings['waifuDraggable'] = 'disable'; // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
         live2d_settings['homePageUrl'] = '/'; // 主页地址，可选 'auto'(自动), '{URL 网址}'
         live2d_settings['showToolMenu'] = false; // 显示 工具栏          ，可选 true(真), false(假)
-        // live2d_settings['canTakeScreenshot'] = false; // 显示 看板娘截图  按钮，可选 true(真), false(假)
-        // live2d_settings['canTurnToAboutPage'] = false; // 显示 跳转关于页  按钮，可选 true(真), false(假)
-        // live2d_settings['canSwitchModel'] = false; // 显示 模型切换    按钮，可选 true(真), false(假)
-
+        live2d_settings['canTakeScreenshot'] = false; // 显示 看板娘截图  按钮，可选 true(真), false(假)
+        live2d_settings['canTurnToAboutPage'] = false; // 显示 跳转关于页  按钮，可选 true(真), false(假)
+        live2d_settings['canSwitchModel'] = false; // 显示 模型切换    按钮，可选 true(真), false(假)
         /* 在 initModel 前添加 */
         initModel('//cdn.jsdelivr.net/gh/juezho/cdn/live2d/waifu-tips.json');
+    } catch {
+        setTimeout(function () {
+            live2d_try()
+        }, 5000)
     }
-});
+}()
